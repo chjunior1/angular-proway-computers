a80be0b31d71c4b8967cbaecfa418b4f215ca88b
Carlos Henrique Galves Junior <chjunior1@gmail.com>
2024-07-09 12:08:08 -0300

    Aula 6.E - criando o arquivo com a tipagem e a listagem dos produtos e os inserindo na pagina produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {},
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: "", redirectTo: "produtos", pathMatch: "full"},
 ];
 
 
